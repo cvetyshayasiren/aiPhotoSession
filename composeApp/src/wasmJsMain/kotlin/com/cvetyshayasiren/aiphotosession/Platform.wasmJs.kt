fb6fb25ffda83224051eb5d0e563132fa867866c
cvetyshayasiren <75530391+cvetyshayasiren@users.noreply.github.com>
2025-11-01 15:38:41 +0000

    hello sample

--- composeApp/src/wasmJsMain/kotlin/com/cvetyshayasiren/aiphotosession/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.cvetyshayasiren.aiphotosession
-
-class WasmPlatform : Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
